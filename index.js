4842fc937e5602f4b4bd30d6d90114a464e4ba66
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-11-22 23:08:45 -0300

    Desafio de produto com graphql

--- index.js
+++ index.js
@@ -6,20 +6,69 @@ const {
 const typeDefs = gql `
     #scalar perso
     scalar Date
+
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
+    }
+
     # Pontos de entrada da sua API!
     type Query {
         ola: String
         horaAtual: Date
+        usuarioLogado: Usuario
+        produtoEmDestaque: Produto
     }
 `
 
 const resolvers = {
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real;
+        }
+    },
+    Produto: {
+        precoComDesconto(produto) {
+            if (produto.desconto) {
+                return produto.preco - produto.desconto;
+            } else
+                return produto.preco;
+        }
+    },
     Query: {
         ola() {
             return 'Hello world !'
         },
         horaAtual() {
             return new Date();
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: 'Ana da Web',
+                email: 'fsafha@gmauk,ciom',
+                idade: 23,
+                salario_real: 1234.56,
+                vip: true
+            }
+        },
+        produtoEmDestaque() {
+            return {
+                nome: 'Conta',
+                preco: 2000,
+                desconto: 230,
+            }
         }
     }
 }
