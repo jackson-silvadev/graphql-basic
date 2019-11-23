124958a69196e63bc33b232c9d75b3403b2a424b
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-11-23 09:41:40 -0300

    Finalizando o projeto basico de consultas

--- data/db.js
+++ data/db.js
@@ -3,21 +3,23 @@ const usuarios = [{
         nome: 'Ana da Web',
         email: 'fsafha@gmauk,ciom',
         idade: 23,
-        perfil_id: 1
-
+        perfil_id: 1,
+        status: 'ATIVO'
     }, {
         id: 2,
         nome: 'Rafa',
         email: 'dad@gmauadak,ciaom',
         idade: 25,
-        perfil_id: 2
+        perfil_id: 2,
+        status: 'INATIVO'
     },
     {
         id: 3,
         nome: 'Web',
         email: 'sdf@gmaukaa,cioam',
         idade: 26,
-        perfil_id: 1
+        perfil_id: 1,
+        status: 'BLOQUEADO'
     }
 ]
 
